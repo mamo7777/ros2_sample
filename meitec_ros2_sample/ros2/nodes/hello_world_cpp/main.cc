56e0cc80e2b10b3ca9d66fb74b561bf7dfcd33e3
Mamoru Uchiuda <m-uchiuda@gr.meitec.com>
2025-02-06 09:55:05 +0900

    add client

--- meitec_ros2_sample/ros2/nodes/hello_world_cpp/main.cc
+++ meitec_ros2_sample/ros2/nodes/hello_world_cpp/main.cc
@@ -7,6 +7,7 @@
 // https://github.com/ros2/common_interfaces/tree/jazzy
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_srvs/srv/trigger.hpp>
+#include <turtlesim/srv/spawn.hpp>
 
 class CppTestNode : public rclcpp::Node {
  public:
@@ -19,6 +20,9 @@ class CppTestNode : public rclcpp::Node {
   void chg_dir_srv_callback(const std::shared_ptr<std_srvs::srv::Trigger::Request> req,
                             const std::shared_ptr<std_srvs::srv::Trigger::Response> res);
 
+  bool check_turtle2();
+  void spawn_turtle2();
+
   uint64_t counter_ = 0;
   int8_t turn_direction_ = 1;
 
@@ -27,6 +31,7 @@ class CppTestNode : public rclcpp::Node {
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel2_publisher_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr turtle_1_cmd_vel_sub_;
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr change_direction_service_;
+  rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawn_cli_;
 };
 
 CppTestNode::CppTestNode() : Node("cpp_test_node") {
@@ -42,6 +47,8 @@ CppTestNode::CppTestNode() : Node("cpp_test_node") {
   change_direction_service_ = create_service<std_srvs::srv::Trigger>(
       "/turtle1/change_direction", std::bind(&CppTestNode::chg_dir_srv_callback, this,
                                              std::placeholders::_1, std::placeholders::_2));
+
+  spawn_cli_ = this->create_client<turtlesim::srv::Spawn>("/spawn");
 }
 
 void CppTestNode::timer_callback() {
@@ -60,6 +67,10 @@ void CppTestNode::timer_callback() {
   message.linear.x = 1.0;
   message.angular.z = 1.0 * turn_direction_;
   vel1_publisher_->publish(message);
+
+  if (!check_turtle2()) {
+    spawn_turtle2();
+  }
 }
 
 void CppTestNode::vel1_callback(const std::shared_ptr<geometry_msgs::msg::Twist> msg) {
@@ -75,6 +86,36 @@ void CppTestNode::chg_dir_srv_callback(
   turn_direction_ *= -1;
   res->success = true;
   res->message = "Success";
+}
+
+bool CppTestNode::check_turtle2() {
+  auto topic_names = this->get_topic_names_and_types();
+  for (const auto& topic : topic_names) {
+    if (topic.first == "/turtle2/pose") {
+      return true;
+    }
+  }
+  return false;
+}
+
+void CppTestNode::spawn_turtle2() {
+  auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
+  request->x = 2.5;
+  request->y = 4.0;
+  request->theta = 0.0;
+  request->name = "turtle2";
+
+  while (!spawn_cli_->wait_for_service(std::chrono::seconds(1))) {
+    SPDLOG_WARN("Waiting for /spawn service...");
+  }
+
+  auto result = spawn_cli_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) ==
+      rclcpp::FutureReturnCode::SUCCESS) {
+    SPDLOG_INFO("Spawned turtle2 successfully.");
+  } else {
+    SPDLOG_ERROR("Failed to spawn turtle2.");
+  }
 }
 
 int main(int argc, char* argv[]) {
