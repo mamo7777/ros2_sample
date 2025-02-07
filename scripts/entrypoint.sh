c0c2abcdbd4628c6a07c9c31a742816bed523a63
Mamoru Uchiuda <m-uchiuda@gr.meitec.com>
2025-02-07 12:49:43 +0900

    update cmd_memo

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,9 +5,9 @@ source /opt/ros/jazzy/setup.bash
 
 BUILD_DIR="/root/ros2_sample/meitec_ros2_sample/build"
 
-if [ ! -d "$BUILD_DIR" ]; then
-  cd /root/ros2_sample/meitec_ros2_sample
-  colcon build
-fi
+#if [ ! -d "$BUILD_DIR" ]; then
+#  cd /root/ros2_sample/meitec_ros2_sample
+#  colcon build
+#fi
 
 exec tmux
