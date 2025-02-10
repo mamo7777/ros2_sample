16e482c45682ad53b0ede11a054eacb2159a5408
Mamoru Uchiuda <m-uchiuda@gr.meitec.com>
2025-02-10 17:33:11 +0900

    fix

--- meitec_ros2_sample/ros2/nodes/hello_world_py/hello_world.py
+++ meitec_ros2_sample/ros2/nodes/hello_world_py/hello_world.py
@@ -12,28 +12,28 @@ class PyTestNode(Node):
     def __init__(self) -> None:
         super().__init__("py_test_node")
 
-        self.timer_period = 1.0
-        self.counter = 0
+        self._timer_period = 1.0
+        self._counter = 0
 
         # タイマーコールバックの初期化
-        self.timer = self.create_timer(self.timer_period, self._timer_callback)
+        self._timer = self.create_timer(self._timer_period, self._timer_callback)
 
     # タイマーコールバック
     def _timer_callback(self) -> None:
         # Errorメッセージ
-        if self.counter >= 10:
-            self.get_logger().error(f"[{self.counter}] Hello, World from PY!")
+        if self._counter >= 10:
+            self.get_logger().error(f"[{self._counter}] Hello, World from PY!")
             self.destroy_node()
             rclpy.shutdown()
             return
         # Warningメッセージ
-        elif self.counter >= 5:
-            self.get_logger().warn(f"[{self.counter}] Hello, World from PY!")
+        elif self._counter >= 5:
+            self.get_logger().warn(f"[{self._counter}] Hello, World from PY!")
         # Infoメッセージ
         else:
-            self.get_logger().info(f"[{self.counter}] Hello, World from PY!")
+            self.get_logger().info(f"[{self._counter}] Hello, World from PY!")
 
-        self.counter = self.counter + 1
+        self._counter = self._counter + 1
 
 
 # main関数
